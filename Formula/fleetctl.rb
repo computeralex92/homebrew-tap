ffae22ba1f8351ef7d3c2896619bba64cc4cf575
Alexander Lauster <alexander@macwinlin.de>
2026-06-07 10:47:38 +0200

    Use FLEET_VERSION constant to avoid repeating version in URLs

--- Formula/fleetctl.rb
+++ Formula/fleetctl.rb
@@ -1,19 +1,20 @@
 class Fleetctl < Formula
+  FLEET_VERSION = "4.86.1".freeze
   desc "Command-line interface for Fleet Device Management"
   homepage "https://fleetdm.com"
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/fleetdm/fleet/releases/download/fleet-v#{version}/fleetctl_v#{version}_macos.zip"
+    url "https://github.com/fleetdm/fleet/releases/download/fleet-v#{FLEET_VERSION}/fleetctl_v#{FLEET_VERSION}_macos.zip"
     sha256 "ab6510afc7686f5416596da16476b5a996dfa86d5b7dded6d77f85ff228f96a0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/fleetdm/fleet/releases/download/fleet-v#{version}/fleetctl_v#{version}_linux_amd64.zip"
+      url "https://github.com/fleetdm/fleet/releases/download/fleet-v#{FLEET_VERSION}/fleetctl_v#{FLEET_VERSION}_linux_amd64.zip"
       sha256 "2883b8166c8b09a584de75708558c926b74df4eab48229972b098d40570952d0"
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/fleetdm/fleet/releases/download/fleet-v#{version}/fleetctl_v#{version}_linux_arm64.zip"
+      url "https://github.com/fleetdm/fleet/releases/download/fleet-v#{FLEET_VERSION}/fleetctl_v#{FLEET_VERSION}_linux_arm64.zip"
       sha256 "92ceee1089c4be0d74afc5289f6e3faa165efb38d4a1a9059fe06c13794cfbbd"
     end
   end
