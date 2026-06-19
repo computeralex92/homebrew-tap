87d45c0d891be5225ef9150d9556c249ab55d671
Alexander Lauster <alexander@macwinlin.de>
2026-06-19 08:09:56 +0200

    Revert "Use #{version} interpolation in URLs for Renovate auto-update (#14)"
    This reverts commit 4dbf229294b5b8bb555f4c74f0bd8e02d2c8a741.

--- Formula/fleetctl.rb
+++ Formula/fleetctl.rb
@@ -1,10 +1,22 @@
 class Fleetctl < Formula
   desc "Command-line interface for Fleet Device Management"
   homepage "https://fleetdm.com"
-
-  url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.86.1/fleetctl_v4.86.1_macos.tar.gz"
-  sha256 "d4f7db86dcb60dc241177505819c20619401dbbbd6003c0091a2888e1d921d36"
   license "MIT"
+
+  if OS.mac?
+    url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.86.1/fleetctl_v4.86.1_macos.tar.gz"
+    sha256 "d4f7db86dcb60dc241177505819c20619401dbbbd6003c0091a2888e1d921d36"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.86.1/fleetctl_v4.86.1_linux_amd64.tar.gz"
+      sha256 "0c5b031e5e973e3f8f96307cfdaa55a33a0ab4e7859eee52150bda51572c7349"
+    end
+
+    if Hardware::CPU.arm?
+      url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.86.1/fleetctl_v4.86.1_linux_arm64.tar.gz"
+      sha256 "eab4e7814d6bd76ec07b662833a0289ecade4b9226efb24b8727459bfc3f3fcc"
+    end
+  end
 
   livecheck do
     url "https://github.com/fleetdm/fleet/releases"
