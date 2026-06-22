8aa6d00e7668547a969659afb49127bfaf1633aa
Alexander Lauster <alexander@macwinlin.de>
2026-06-22 22:22:45 +0200

    Restore blank lines in fleetctl.rb for style

--- Formula/fleetctl.rb
+++ Formula/fleetctl.rb
@@ -4,12 +4,14 @@ class Fleetctl < Formula
   url "https://github.com/fleetdm/fleet/archive/refs/tags/fleet-v4.87.0.tar.gz"
   sha256 "e936c930ae08e564488b677317507fc8c24ccfeaa01c5be9a16842c6fe884c61"
   license "MIT"
+
   livecheck do
     url :stable
     regex(/^fleet-v?(\d+(?:\.\d+)+)$/i)
   end
 
   depends_on "go" => :build
+
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/fleetctl"
   end
