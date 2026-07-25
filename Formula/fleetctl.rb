c56b80ecc1dcd2a4987c96c6919557048bfe3c07
Anka <runner@iad20-eo1213-0ccdd115-3136-4263-97e3-6be8d2a0cb85-DAAD0C04CA96.local>
2026-07-25 08:11:02 +0000

    fleetctl 4.89.2

--- Formula/fleetctl.rb
+++ Formula/fleetctl.rb
@@ -1,8 +1,8 @@
 class Fleetctl < Formula
   desc "Command-line interface for Fleet Device Management"
   homepage "https://fleetdm.com"
-  url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.89.1/fleetctl_v4.89.1_macos.tar.gz"
-  sha256 "a13f88800e59792af3480feb1ac0e6fe4f63775e87f237b5a47264a5e05f85e6"
+  url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.89.2/fleetctl_v4.89.2_macos.tar.gz"
+  sha256 "38222bb2a5cb010e3de1337e6cd500a4d8773f68da1f74696850ed4ec1b18270"
   license "MIT"
 
   livecheck do
