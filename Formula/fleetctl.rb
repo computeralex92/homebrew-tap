de6f1868ee621d99448bba01bc4e3ccf56512fcf
Alexander Lauster <alexander@macwinlin.de>
2026-08-15 08:47:51 +0200

    fleetctl 4.90.1 (#51)
    Co-authored-by: Anka <runner@sat12-bq146-8e049923-8308-41d9-ac19-8151267b9d74-26A250D8B49B.local>

--- Formula/fleetctl.rb
+++ Formula/fleetctl.rb
@@ -1,8 +1,8 @@
 class Fleetctl < Formula
   desc "Command-line interface for Fleet Device Management"
   homepage "https://fleetdm.com"
-  url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.90.0/fleetctl_v4.90.0_macos.tar.gz"
-  sha256 "02e171f89d8146c5f732b9f1b3569a9acbf874db89fff063d3cc82de9b5bcd6c"
+  url "https://github.com/fleetdm/fleet/releases/download/fleet-v4.90.1/fleetctl_v4.90.1_macos.tar.gz"
+  sha256 "439e00f33710f57d4586b0141dce3c032fc3eb4c1997550de80568e8bfa8bbc2"
   license "MIT"
 
   livecheck do
